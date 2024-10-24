cb9da0f6cb17fc6ccc01f5d1390d36d0cac96672
Robert_Jean <stevejean285@gmail.com>
2024-10-23 23:08:46 -0400

    Partialially working trampoline

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-let counter = 0;
+// let counter = 0;
 
 
-function foo()
-{
-    counter++;
-    return foo();
-}
+// function foo() {
+//     counter++;
+//     return foo();
+// }
 
+// try {
+//     foo();
+// }
+// catch (err) {
+//     console.log(err);
+//     console.log(counter);
+// }
 
-try{
-    foo();
-}
-catch(err)
-{
-    console.log(err);
-    console.log(counter);
-}
+// recursively flaten the arrray. 
+const numberArray = [1, 2, 4, [4, 7, 8, [5, 6, 7], [[[6, 7], [7, 8]], [4, 5, 6]], 7, 8], 8, 8];
 
-// recursively flaaten the arrray. 
 function flatten(arr) {
     const result = [];
     arr.forEach(element => {
@@ -29,3 +28,38 @@ function flatten(arr) {
     });
     return result;
 }
+
+console.log(flatten(numberArray));
+
+//modify the recursive function to return a function 
+function flattenArray(arr) {
+
+    let result = [];
+    function checkForNested(ar, index, res) {
+
+        if (index >= ar.length) {
+            return res;
+        }
+        let item = ar[index];
+        if (Array.isArray(item)) {
+            return () => {item.forEach((subItem, subIndex) => checkForNested(item, subIndex, res));
+                return checkForNested(arr, index + 1, res)};
+        } else {
+            res.push(item);
+            return () => checkForNested(ar, index + 1, res);
+        }
+    }
+    return () => checkForNested(arr, 0, result);
+}
+
+// function trampoline 
+function trampoline(fn) {
+    let thunks = fn;
+    while (typeof thunks === 'function') {
+        thunks = thunks();
+    }
+    return thunks;
+}
+// testing implementation on 
+const numberArray2 = [1, [2, [3, 4], 5], 6];
+console.log(trampoline(flattenArray(numberArray2)));
